6090a80a4ad269875399b094d6e51e9d158a9a69
daniel de brito <danieldebrito@outlook.com>
2024-05-15 00:03:06 -0300

    juegos

--- src/app/pages/juegos/mayormenor/mayormenor.component.ts
+++ src/app/pages/juegos/mayormenor/mayormenor.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { User } from '../../../auth/models/user';
+import { Naipe } from '../../../class/naipe';
+import { Score } from '../../../class/score';
+import { NaipesService } from '../../../services/naipesJSON.service';
+import { ScoresService } from '../../../services/scores.FIRE.service';
 
 @Component({
   selector: 'app-mayormenor',
   templateUrl: './mayormenor.component.html',
-  styleUrl: './mayormenor.component.scss'
+  styleUrls: ['./mayormenor.component.scss']
 })
-export class MayormenorComponent {
+export class MayormenorComponent implements OnInit {
+  
 
+  public score: Score = {};
+  public scores: Score[] = [];
+
+  public currentUser: User = {};
+
+  public naipes: Naipe[] = [];
+  cartaAleatoria: Naipe = {};
+  cartaAleatoriaAnterior: Naipe = {};
+  indiceActual: number = 0;
+  public mensaje: string = '';
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private cnaipesSv: NaipesService,
+    private scoresSv: ScoresService) { }
+
+  obtenerNaipeAleatorio() {
+    if (this.naipes && this.indiceActual < this.naipes.length) {
+      this.cartaAleatoria = this.naipes[this.indiceActual];
+      this.indiceActual++;
+    }
+  }
+
+  public obtenerTodosLosNaipesAleatorios() {
+    this.cnaipesSv.obtenernaipesAleatorias().subscribe((naipes: Naipe[]) => {
+      this.naipes = naipes;
+      this.indiceActual = 0;
+
+      this.obtenerNaipeAleatorio();
+    });
+  }
+
+  public checkJugada(jugada: string) {
+
+    let resultado: string = '';
+    this.cartaAleatoriaAnterior = this.cartaAleatoria;
+
+    if (this.naipes && this.indiceActual < this.naipes.length) {
+      this.cartaAleatoria = this.naipes[this.indiceActual];
+      this.indiceActual++;
+
+      if ((this.cartaAleatoriaAnterior.valor !== undefined && this.cartaAleatoria.valor !== undefined) && (this.cartaAleatoriaAnterior.valor > this.cartaAleatoria.valor)) {
+        resultado = 'menor'
+      } else {
+        resultado = 'mayor'
+      }
+
+      if (jugada == resultado) {
+        this.mensaje = 'Bien!!';
+        this.score
+      } else {
+        this.mensaje = 'Perdiste!!';
+      }
+
+    }
+  }
+
+  public getScoreUsuario(uid: string) {
+    this.scoresSv.getItems().subscribe(res => {
+      this.score = res.find(e => e.uid == this.currentUser.uid  ) ?? {};
+    });
+  }
+
+  private getCurretUser() {
+    /*
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.currentUser = user;
+      } else {
+        this.currentUser = {};
+      }
+    });
+    */
+  }
+
+  ngOnInit() {
+    this.obtenerTodosLosNaipesAleatorios();
+    this.getCurretUser();
+  }
 }
--- src/app/services/scores.FIRE.service.ts
+++ src/app/services/scores.FIRE.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
-import { collection, collectionData, deleteDoc, doc, docData, Firestore, orderBy, query, setDoc, updateDoc } from '@angular/fire/firestore';
+import {
+  collection,
+  collectionData,
+  deleteDoc,
+  doc,
+  docData,
+  Firestore,
+  orderBy,
+  query,
+  setDoc,
+  updateDoc,
+} from '@angular/fire/firestore';
 import { catchError, map, Observable, throwError } from 'rxjs';
-import { Score } from 'src/app/class/score';
+import { Score } from '../class/score';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ScoresService {
-
-  constructor(private firestore: Firestore) { }
+  constructor(private firestore: Firestore) {}
 
   public addItem(item: Score) {
     const col = collection(this.firestore, 'Scores');
@@ -22,10 +32,10 @@ export class ScoresService {
     const col = collection(this.firestore, 'Scores');
     const queryObservable = query(col, orderBy('uid')); // ordenar por fecha
     const observable = collectionData(queryObservable).pipe(
-      map(res => {
+      map((res) => {
         return res as Score[];
       }),
-      catchError(err => {
+      catchError((err) => {
         console.error('Error obteniendo datos:', err);
         return throwError(() => err);
       })
@@ -38,10 +48,10 @@ export class ScoresService {
     const documento = doc(col, id);
 
     const observable = docData(documento).pipe(
-      map(res => {
+      map((res) => {
         return res as Score;
       }),
-      catchError(err => {
+      catchError((err) => {
         console.error('Error obteniendo el documento:', err);
         return throwError(() => err);
       })
@@ -66,9 +76,9 @@ export class ScoresService {
   ////////////////////////////////////////////////////////////////////////////
 
   public getScoreUsuario(): any {
-
+    /*
     let scoreTEMP: Score = {};
-    const user = JSON.parse(localStorage.getItem('user'));
+    //const user = JSON.parse(localStorage.getItem('user'));
 
     this.getItems().subscribe( res => {
 
@@ -85,5 +95,6 @@ export class ScoresService {
 
       return scoreTEMP;
     });
+    */
   }
 }
